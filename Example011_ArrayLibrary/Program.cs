bffe590c32d9fa8fc82319d6267980b65a2ca0fd
Maou <waspofcrimea@yandex.ru>
2022-11-20 21:34:09 +0300

    solved Indexof

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -20,20 +20,20 @@ void PrintArray(int[] col)
 }
 int IndexOf(int[] collection, int find)
 {
-    int count = collection.Length;
-    int index = 0;
-    int position = 0;
-    while (index < count)
-   
-       { if(collection[index] == find)
-        {
-            position = index;
-            index++;
-        }
-        return position;
-       
-    }
+int index = collection.Length;
+int count = 0;
+int position = 0;
+while (index < count)
+{
+ if(collection[index] == find)
+ {
+ position = index;
+ }
+index++;
 }
+return position;
+}
+
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
