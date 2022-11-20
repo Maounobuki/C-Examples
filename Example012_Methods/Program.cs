1f601546d70c2fea407faf1918618ed5851b2dd7
Maou <waspofcrimea@yandex.ru>
2022-11-20 21:51:44 +0300

    added all methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -35,3 +35,82 @@ return result;
 }
 string res = Metod4(10, "asdf");
 Console.WriteLine(res);
+
+string Metod4(int count, string text)
+{
+string result = String.Empty;
+for (int i = 0; i<count; i++) /*- вначале ключевое слово, затем инициализация
+счётчика, после проверка условия и инкремент (увеличение счётчика).*/
+{
+result = result + text;
+}
+return result;
+}
+string res = Metod4(10, "asdf");
+Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+for (int j = 2; j <= 10; j++)
+{
+Console.WriteLine($"{i} * {j} = {i * j}");
+}
+Console.WriteLine();
+}
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+// string s = “qwerty”
+// 012345
+string Replase(string text, char oldValue, char newValue)
+{
+string result = String.Empty;
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+if(text[i] ==oldValue) result + $"{NewValue}";
+else result + $"{str[i]}";
+}
+return result;
+}
+string newText = replace(text, ' ',  '|' );
+Console.WriteLine(newText);
+string newText = Replace(text, ' ', '|' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PtintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+}
+void selectionSort(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for (int j = i+1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition]) minPosition = j;
+{
+minPosition = j;
+}
+}
+int temporary = array[i];
+array[i] = array[minPosition];
+array[minPosition] = temporary;
+}
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
